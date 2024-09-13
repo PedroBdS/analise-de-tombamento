6e89ba2b87cbb9a181c7a39a2ef0fd23578cf8a5
Silva <PBOTTARI@ball.com>
2024-09-13 08:33:27 -0300

    org

---
+++ treino/carregar_modelo_h5.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+import io
+from PIL import Image
+import numpy as np
+import pandas as pd
+import plotly.express as px
+import keras
+
+path = "./treino/modelos/alura_tombamento_modelo.h5"
+
